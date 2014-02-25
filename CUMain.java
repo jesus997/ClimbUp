73bb43dd5faf915aa7eaefad3f63a003a07c3c81
jesus997 <jesus9711@live.com.mx>
2014-02-24 18:39:40 -0600

    Agregadas correcciones

--- CUMain.java
+++ CUMain.java
@@ -1,22 +1,26 @@
 package me.jesus997.ClimbUp;
 
-import java.util.List;
+import org.bukkit.GameMode;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CUMain extends JavaPlugin{
+	private ArenaManager a;
+        private static CUMain instance;
     @Override
     public void onEnable(){
+        instance = this;
         getServer().getPluginManager().registerEvents(new GameListener(this), this);
         if(!getDataFolder().exists()) 
             getDataFolder().mkdir();
-        if(getConfig() == null)
-            ArenaManager.getManager().createConfig();
-            saveDefaultConfig();
+        a = new ArenaManager(this);
+
+        //if(getConfig() == null)
+            a.createConfig();
+            //saveDefaultConfig();
         
-        ArenaManager a = new ArenaManager(this);
         
         getLogger().info("ClimbUp activado correctemente!");
     }
@@ -28,30 +32,57 @@ public class CUMain extends JavaPlugin{
     
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
-        if(cmd.getName().equalsIgnoreCase("cu")){
+    	if(sender.getName().equalsIgnoreCase("CONSOLE")){return true;}
+    	if(cmd.getName().equalsIgnoreCase("cu")){
+        	if(args.length<2){
+        		return true;
+        	}
             if(args[0].equalsIgnoreCase("create")){
+            	if(args.length<3){
+            		return true;
+            	}
                 if(args[1].equalsIgnoreCase("lobby")){
+                    if(args.length < 4 ){
+                        return true;
+                    }
                     Player p = (Player) sender;
-                    int maxPlayer =  Integer.parseInt(args[2]);
-                    ArenaManager.getManager().createLobby(p.getLocation(), maxPlayer);
+                    int id =  Auxiliar.getNatural(args[2], -1);
+                    int maxP = Integer.parseInt(args[3]);
+                    a.createLobby(p.getLocation(), id, maxP);
+                    p.sendMessage("Arena creada con id: " + id + ", para " + maxP + " jugadores.");
                 }else if(args[1].equalsIgnoreCase("meta")){
                     Player p = (Player) sender;
-                    int id = Integer.parseInt(args[2]);
-                    ArenaManager.getManager().createMeta(p.getLocation(), id);
+                    int id = Auxiliar.getNatural(args[2], -1);
+                    a.createMeta(p.getLocation(), id);
+                    p.sendMessage("Meta creada para la arena " + id + "."); 
                 }else if(args[1].equalsIgnoreCase("respawn")){
                     Player p = (Player) sender;
-                    int num = Integer.parseInt(args[2]);
-                    ArenaManager.getManager().createRespawn(p.getLocation(), num);
+                    int id = Auxiliar.getNatural(args[2], -1);
+                    a.createRespawn(p.getLocation(), id, p);
                 }
             }else if(args[0].equalsIgnoreCase("join")){
                 Player p = (Player) sender;
-                int id = Integer.parseInt(args[1]);
-                ArenaManager.getManager().addPlayer(p, id);
+                int id = Auxiliar.getNatural(args[2], -1);
+                a.addPlayer(p, id);
             }else if(args[0].equalsIgnoreCase("leave")){
                 Player p = (Player) sender;
-                ArenaManager.getManager().removePlayer(p);
+                if(p.getGameMode() == p.getGameMode().ADVENTURE){
+                    p.setGameMode(GameMode.SURVIVAL);
+                }
+                a.removePlayer(p);
+            }else if(args[0].equalsIgnoreCase("spectate")){
+                Player p = (Player) sender;
+                p.setGameMode(GameMode.ADVENTURE);
+            }else if(args[0].equalsIgnoreCase("start")){
+                int id = Auxiliar.getNatural(args[2], -1);
+                a.startArena(id);
             }
         }
         return false;
+    }
+    
+    public ArenaManager getManager(){return a;}
+    public static CUMain getInstance(){
+        return instance;
     }
 }
