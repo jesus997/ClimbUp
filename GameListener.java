0eae84b2042d020242ea5d69d8fec4c39767e933
jesus997 <jesus9711@live.com.mx>
2014-02-24 18:40:19 -0600

    Agregadas correcciones

--- GameListener.java
+++ GameListener.java
@@ -2,16 +2,10 @@ package me.jesus997.ClimbUp;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 import org.bukkit.Bukkit;
-import org.bukkit.Color;
-import org.bukkit.FireworkEffect;
-import org.bukkit.FireworkEffect.Type;
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
 import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.World;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Firework;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -19,9 +13,9 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.player.PlayerGameModeChangeEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
-import org.bukkit.inventory.meta.FireworkMeta;
 
 public class GameListener implements Listener{
     static List<String> players = new ArrayList<String>();
@@ -33,7 +27,7 @@ public class GameListener implements Listener{
     // No tieren las cosas al morir
     @EventHandler
     public void onPlayerDeath(PlayerDeathEvent e){
-        if(ArenaManager.getManager().isInGame(e.getEntity())){
+        if(plugin.getManager().isInGame(e.getEntity())){
             e.getDrops().clear();
         }           
     }
@@ -41,7 +35,7 @@ public class GameListener implements Listener{
     // Evitar que rompan bloques
     @EventHandler
     public void BlockBreakEvent(BlockBreakEvent e){
-        if(ArenaManager.getManager().isInGame(e.getPlayer())){
+        if(plugin.getManager().isInGame(e.getPlayer())){
             e.setCancelled(true);
         }
     }
@@ -49,7 +43,7 @@ public class GameListener implements Listener{
     // Evitar que pungan bloques
     @EventHandler
     public void BlockPlaceEvent(BlockPlaceEvent e){
-        if(ArenaManager.getManager().isInGame(e.getPlayer())){
+        if(plugin.getManager().isInGame(e.getPlayer())){
             e.setCancelled(true);
         }
     }
@@ -57,15 +51,15 @@ public class GameListener implements Listener{
     // Sacarlos de la arena al desconectarse del servidor
     @EventHandler
     public void onLeavePlayerServer(PlayerQuitEvent e){
-        if(ArenaManager.getManager().isInGame(e.getPlayer())){
-            ArenaManager.getManager().removePlayer(e.getPlayer());
+        if(plugin.getManager().isInGame(e.getPlayer())){
+        	plugin.getManager().removePlayer(e.getPlayer());
         }
     }
     
     // No permitir que usen comandos dentro de la arena
     @EventHandler
     public void onPlayerCommandBlock(PlayerCommandPreprocessEvent e){
-        if(ArenaManager.getManager().isInGame(e.getPlayer())){
+        if(plugin.getManager().isInGame(e.getPlayer())){
             if(e.getPlayer().hasPermission("cu.editArena")){
                 e.setCancelled(false);
             }else{
@@ -77,172 +71,79 @@ public class GameListener implements Listener{
         }
     }
     
+    // Create gamemode by ccrama
+    @EventHandler
+    public void onGameModeChange(PlayerGameModeChangeEvent e){
+        if(plugin.getManager().isInGame(e.getPlayer())){
+            Jugador j=plugin.getManager().getJugador(e.getPlayer());
+            if(j==null){return;}
+            Arena a = j.getArena();
+            final Player p = e.getPlayer();
+            if(e.getNewGameMode() == GameMode.ADVENTURE){
+
+
+                  Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable()
+                    {
+                        @Override
+                        public void run()
+                        {
+                            p.setAllowFlight(true);
+                            p.setFlying(true);     
+                            p.teleport(p.getLocation().add(0,5,0));
+
+                        }
+                    }, 2L);
+                for(Player pla: Bukkit.getOnlinePlayers()){
+                    pla.hidePlayer(e.getPlayer());
+                }
+
+                e.getPlayer().sendMessage(ChatColor.RED + "Ahora eres un espectador!");
+
+
+            } else { //will fire when player is changed to SURVIVAL or CREATIVE gamemodes
+              for(Player pla:Bukkit.getOnlinePlayers()){ //reshows a player to all online players
+                    pla.showPlayer(e.getPlayer());
+
+                }
+                e.getPlayer().setAllowFlight(false); //disallows flight
+                e.getPlayer().setFlying(false); //makes them stop flying if they are in flight
+                e.getPlayer().teleport(a.getLobby()); //teleport them to spawn if they are no longer spectator
+            }
+        }
+    }
+    
     // creación de la cupula del jugador y fuegos artificiales
         @EventHandler
      public void onPlayerMove(PlayerMoveEvent e){
-            if(ArenaManager.getManager().isInGame(e.getPlayer())){
-                        Player player = e.getPlayer();
-                        Location loc = player.getLocation();
-                        double locX = Math.floor(loc.getBlockX());
-                        double locY = loc.getBlockY();
-                        double locZ = Math.floor(loc.getBlockZ());
-                        World mundo = Bukkit.getWorld(loc.getWorld().getName());
-                        Arena a = null;
-                        for(Arena arena : ArenaManager.getManager().arenas){
-                            if(arena.getPlayers().contains(e.getPlayer().getName())){
-                                a = arena;
-                            }
-                        }
-                        int id = a.getId();
-                        String s = plugin.getConfig().getString("Arenas." + id + "meta");
-                        Location locP = ArenaManager.getManager().deserializeloc(s);
-                        //double X = getConfig().getDouble("arena1.meta.x");
-                        //double Y = getConfig().getDouble("arena1.meta.y");
-                        //double Z = getConfig().getDouble("arena1.meta.z");
-                        Location locp2 = new Location(mundo,locX, locY, locZ);
-                        if(locP == locp2){
-                        //if(locX == X && locY == Y && locZ == Z ){
-                            final World world = player.getWorld();
-                            final int x = (int)locP.getBlockX();
-                            final int y = (int)locP.getBlockY();
-                            final int z = (int)locP.getBlockZ();
+            if(plugin.getManager().isInGame(e.getPlayer())){
+                        final Player player = e.getPlayer();
+               
+                        final Jugador j=plugin.getManager().getJugador(e.getPlayer());
+                        if(j==null){return;}
+                        final Arena a=j.getArena();
 
+                        Location locP = a.getMeta();
 
-                            world.getBlockAt(x,y+2,z).setType(Material.GLASS); // Bloque encima del jugador
-                            world.getBlockAt(x-1,y+2,z).setType(Material.GLASS); // Bloque a la derecha del bloque de arriba
-                            world.getBlockAt(x+1,y+2,z).setType(Material.GLASS); // Bloque a la derecha del bloque de arriba
-                            world.getBlockAt(x,y+2,z+1).setType(Material.GLASS); // Bloque que esta delante del bloque de arriba
-                            world.getBlockAt(x,y+2,z-1).setType(Material.GLASS); // Bloque que esta detras del bloque de arriba
-
-                            world.getBlockAt(x+1,y-1,z).setType(Material.GLASS); // Bloque al frente del de abajo
-                            world.getBlockAt(x-1,y-1,z).setType(Material.GLASS); // Bloque tracero al de abajo
-                            world.getBlockAt(x,y-1,z+1).setType(Material.GLASS); // Bloque a la derecha de abajo
-                            world.getBlockAt(x,y-1,z-1).setType(Material.GLASS); // Bloque a la izquierda de abajo
-
-                            world.getBlockAt(x+2,y,z).setType(Material.GLASS);  // Bloque 1 columna +x
-                            world.getBlockAt(x+2,y+1,z).setType(Material.GLASS);  // Bloque 2 columna +x
-
-                            world.getBlockAt(x-2,y,z).setType(Material.GLASS);  // Bloque 1 columna -x
-                            world.getBlockAt(x-2,y+1,z).setType(Material.GLASS);  // Bloque 2 columna -x   
-
-                            world.getBlockAt(x,y,z+2).setType(Material.GLASS);  // Bloque 1 columna +z
-                            world.getBlockAt(x,y+1,z+2).setType(Material.GLASS);  // Bloque 2 columna +z
-
-                            world.getBlockAt(x,y,z-2).setType(Material.GLASS);  // Bloque 1 columna -z
-                            world.getBlockAt(x,y+1,z-2).setType(Material.GLASS);  // Bloque 2 columna -z  
-
-                            world.getBlockAt(x+1,y,z+1).setType(Material.GLASS);  // Bloque 1 columna +x
-                            world.getBlockAt(x+1,y+1,z+1).setType(Material.GLASS);  // Bloque 2 columna +x
-
-                            world.getBlockAt(x-1,y,z-1).setType(Material.GLASS);  // Bloque 1 columna -x
-                            world.getBlockAt(x-1,y+1,z-1).setType(Material.GLASS);  // Bloque 2 columna -x   
-
-                            world.getBlockAt(x+1,y,z-1).setType(Material.GLASS);  // Bloque 1 columna +z
-                            world.getBlockAt(x+1,y+1,z-1).setType(Material.GLASS);  // Bloque 2 columna +z
-
-                            world.getBlockAt(x-1,y,z+1).setType(Material.GLASS);  // Bloque 1 columna -z
-                            world.getBlockAt(x-1,y+1,z+1).setType(Material.GLASS);  // Bloque 2 columna -z    
-
-                            // Fuego artificial
-
-                             //Spawn the Firework, get the FireworkMeta.
-                            Firework fw = (Firework) player.getWorld().spawnEntity(player.getLocation(), EntityType.FIREWORK);
-                            FireworkMeta fwm = fw.getFireworkMeta();
-
-                            //Our random generator
-                            Random r = new Random();   
-
-                            //Get the type
-                            int rt = r.nextInt(5) + 1;
-                            Type type = Type.BALL;       
-                            if (rt == 1) type = Type.BALL;
-                            if (rt == 2) type = Type.BALL_LARGE;
-                            if (rt == 3) type = Type.BURST;
-                            if (rt == 4) type = Type.CREEPER;
-                            if (rt == 5) type = Type.STAR;
-
-                            //Get our random colours   
-                            int r1i = r.nextInt(17) + 1;
-                            int r2i = r.nextInt(17) + 1;
-                            Color c1 = getColor(r1i);
-                            Color c2 = getColor(r2i);
-
-                            //Create our effect with this
-                            FireworkEffect effect = FireworkEffect.builder().flicker(r.nextBoolean()).withColor(c1).withFade(c2).with(type).trail(r.nextBoolean()).build();
-
-                            //Then apply the effect to the meta
-                            fwm.addEffect(effect);
-
-                            //Generate some random power and set it
-                            int rp = r.nextInt(1) + 1;
-                            fwm.setPower(rp);
-
-                            //Then apply this to our rocket
-                            fw.setFireworkMeta(fwm);
-
+                        if(locP.distance(player.getLocation())<2){
+                            Auxiliar.acristalar(player.getLocation());
+                            Auxiliar.FuegosArtificiales(player.getLocation());
 
                             player.sendMessage("Has ganado la primera ronda!");
-                            player.teleport(a.spawn);
+                            //player.teleport(a.getSpawn());       
+                            
+                            Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, new Runnable(){
+                                @Override
+                                public void run(){
+                                    for(Player p :Bukkit.getOnlinePlayers()){
+                                            p.teleport(j.getSpawn());
+                                    }
+                                    Auxiliar.desAcristalar(player.getLocation());
+                                    
+                                }
+                            }, 100L);
                         }
             }
     }
-    
-     private Color getColor(int i) {
-                Color c = null;
-                if(i==1){
-                c=Color.AQUA;
-                }
-                if(i==2){
-                c=Color.BLACK;
-                }
-                if(i==3){
-                c=Color.BLUE;
-                }
-                if(i==4){
-                c=Color.FUCHSIA;
-                }
-                if(i==5){
-                c=Color.GRAY;
-                }
-                if(i==6){
-                c=Color.GREEN;
-                }
-                if(i==7){
-                c=Color.LIME;
-                }
-                if(i==8){
-                c=Color.MAROON;
-                }
-                if(i==9){
-                c=Color.NAVY;
-                }
-                if(i==10){
-                c=Color.OLIVE;
-                }
-                if(i==11){
-                c=Color.ORANGE;
-                }
-                if(i==12){
-                c=Color.PURPLE;
-                }
-                if(i==13){
-                c=Color.RED;
-                }
-                if(i==14){
-                c=Color.SILVER;
-                }
-                if(i==15){
-                c=Color.TEAL;
-                }
-                if(i==16){
-                c=Color.WHITE;
-                }
-                if(i==17){
-                c=Color.YELLOW;
-                }
-
-                return c;
-}
     
     public static void add(Player p){
         final String name = p.getName();
