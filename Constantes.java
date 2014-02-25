0fb8d85fac828ea8c672790c5a23f0973c8b9fcb
jesus997 <jesus9711@live.com.mx>
2014-02-24 18:43:35 -0600

    Agregada clase 'Constantes'

---
+++ Constantes.java
@@ -0,0 +1,8 @@
+package me.jesus997.ClimbUp;
+
+public class Constantes {
+	public static enum Estado{
+		Iniciando, EnCurso, Finalizada,
+		NoDisponible, EsperandoJugadores, SinIniciar, Regenerando
+	}
+}
